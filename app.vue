3d51d92526740d8b0a2d3161d6101e6749a5e819
rlabrovi <roko.labrovic@gmail.com>
2022-10-05 18:15:09 +0200

    Adding Netifly CMS

--- app.vue
+++ app.vue
@@ -1,8 +1,22 @@
-<!-- ./app.vue -->
 <template>
+    <Head>
+        <Script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></Script>
+    </Head>
+
     <div class="nuxt">
         <SiteHeader />
         <NuxtPage />
         <Footer />
     </div>
 </template>
+
+<script setup>
+    useHead({
+        script: [
+            {
+                children:
+                    'if (window.netlifyIdentity) { window.netlifyIdentity.on("init", user => { if (!user) { window.netlifyIdentity.on("login", () => { document.location.href = "/admin/"; }); } }); }',
+            },
+        ],
+    });
+</script>
