c61c5425a3a412e2a6d19c2eb0ce297d8657c7b1
rlabrovi <roko.labrovic@gmail.com>
2022-10-09 00:45:35 +0200

    Load netlify script in body

--- app.vue
+++ app.vue
@@ -1,7 +1,5 @@
 <template>
-    <Head>
-        <Script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></Script>
-    </Head>
+    <Script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></Script>
 
     <div class="nuxt">
         <SiteHeader />
@@ -16,6 +14,7 @@
             {
                 children:
                     'if (window.netlifyIdentity) { window.netlifyIdentity.on("init", user => { if (!user) { window.netlifyIdentity.on("login", () => { document.location.href = "/admin/"; }); } }); }',
+                body: true,
             },
         ],
     });
