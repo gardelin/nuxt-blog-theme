43b9831614d9f929526df005b5f24c65fd2cb0a8
rlabrovi <roko.labrovic@gmail.com>
2022-10-31 20:32:30 +0100

    Adding 404 error page

---
+++ pages/404.vue
@@ -0,0 +1,9 @@
+<template>
+    <section class="flex items-center justify-center text-center w-full h-screen">
+        <header>
+            <h1 class="font-black text-8xl">404</h1>
+            <p>Looks like you are lost!</p>
+            <button>Go back to Home</button>
+        </header>
+    </section>
+</template>
