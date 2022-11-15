501f0c4c414e569c01447519bddbf3374a9edb8b
rlabrovi <roko.labrovic@gmail.com>
2022-11-15 14:38:12 +0100

    Projects page

---
+++ app.config.ts
@@ -0,0 +1,165 @@
+export default defineAppConfig({
+    projects: [
+        {
+            year: 2022,
+            name: 'Lavander',
+            description: 'Nuxt v3 theme',
+            firm: 'Personal',
+            stack: ['Nuxt', 'Vue'],
+            links: [
+                {
+                    type: 'github',
+                    link: 'https://github.com/gardelin/nuxt-blog-theme',
+                },
+            ],
+        },
+        {
+            year: 2022,
+            name: 'Climbing Cardboard',
+            description: 'Wordpress plugin which let users to create climbing card/logbook, record climbed routes and track statistics.',
+            firm: 'Personal',
+            stack: ['WordPress', 'PHP', 'Vue', 'Vuex', 'ChartJS'],
+            links: [
+                {
+                    type: 'github',
+                    link: 'https://github.com/gardelin/climbing-card',
+                },
+            ],
+        },
+        {
+            year: 2022,
+            name: 'Optimizepress Mentor',
+            description: 'Plugin for creating courses and memberships.',
+            firm: 'Optimizepress',
+            stack: ['WordPress', 'PHP', 'Vue', 'Vuex'],
+            links: [
+                {
+                    type: 'website',
+                    link: 'https://www.optimizepress.com/',
+                },
+            ],
+        },
+        {
+            year: '2018 - 2022',
+            name: 'OptimizePress Builder 3',
+            description: 'Page builer',
+            firm: 'Optimizepress',
+            stack: ['WordPress', 'PHP', 'jQuery', 'JS', 'Gulp'],
+            links: [
+                {
+                    type: 'website',
+                    link: 'https://www.optimizepress.com',
+                },
+            ],
+        },
+        {
+            year: '2019 - 2022',
+            name: 'OptimizeFunnels',
+            description: 'Funnel builder',
+            firm: 'Optimizepress',
+            stack: ['WordPress', 'PHP', 'JS', 'ES6', 'Webpack'],
+            links: [
+                {
+                    type: 'website',
+                    link: 'https://www.optimizepress.com/funnels',
+                },
+            ],
+        },
+        {
+            year: '2019 - 2022',
+            name: 'OptimizeDashboard',
+            description: 'Descrtiption',
+            firm: 'Optimizepress',
+            stack: ['WordPress', 'PHP', 'JS', 'ES6', 'Webpack'],
+            links: [
+                {
+                    type: 'website',
+                    link: 'https://www.optimizepress.com/',
+                },
+            ],
+        },
+        {
+            year: '2019 - 2022',
+            name: 'OptimizeCheckouts',
+            description:
+                'Plugin for creating products, services, upsells, downsells, order bumps and other conversion enhancing checkout options. Integrated with Paypal and Stripe.',
+            firm: 'Optimizepress',
+            stack: ['WordPress', 'PHP', 'JS', 'ES6', 'Webpack'],
+            links: [
+                {
+                    type: 'website',
+                    link: 'https://www.optimizepress.com/',
+                },
+            ],
+        },
+        {
+            year: '2019 - 2022',
+            name: 'Optimizepress Integrations',
+            description: 'Email integrations PHP package',
+            firm: 'Optimizepress',
+            stack: ['PHP'],
+            links: [
+                {
+                    type: 'website',
+                    link: 'https://docs.optimizepress.com/article-categories/integrations/',
+                },
+            ],
+        },
+        {
+            year: 2018,
+            name: 'OptimizeLeads',
+            description: 'OptmizeLeads is a stand-alone email list building tool that you can use on any website you own',
+            firm: 'Optimizepress',
+            stack: ['Laravel', 'PHP', 'JS', 'ES6'],
+            links: [
+                {
+                    type: 'website',
+                    link: 'https://www.optimizeleads.com/',
+                },
+                {
+                    type: 'website',
+                    link: 'https://my.optimizeleads.com/',
+                },
+            ],
+        },
+        {
+            year: 2016,
+            name: 'Optimizepress 2 Plus Pack',
+            description: 'Additional elements for legacy page builder',
+            firm: 'Optimizepress',
+            stack: ['WordPress', 'PHP', 'JS', 'jQuery'],
+            links: [
+                {
+                    type: 'website',
+                    link: 'https://www.optimizepress.com/',
+                },
+            ],
+        },
+        {
+            year: 2016,
+            name: 'Optimizepress 2 Theme',
+            description: 'Legacy page builder theme',
+            firm: 'Optimizepress',
+            stack: ['WordPress', 'PHP', 'JS', 'jQuery'],
+            links: [
+                {
+                    type: 'website',
+                    link: 'https://www.optimizepress.com/',
+                },
+            ],
+        },
+        {
+            year: 2016,
+            name: 'Optimizepress 2 Plugin',
+            description: 'Legacy page builder plugin',
+            firm: 'Optimizepress',
+            stack: ['WordPress', 'PHP', 'JS', 'jQuery'],
+            links: [
+                {
+                    type: 'website',
+                    link: 'https://www.optimizepress.com/',
+                },
+            ],
+        },
+    ],
+});
---
+++ pages/projects/index.vue
@@ -0,0 +1,68 @@
+<template>
+    <main class="mx-auto max-w-7xl w-full px-14">
+        <section class="text-center mb-12">
+            <h1 class="page-title">Projects</h1>
+            <p class="page-description font-space-mono">A list of things I've worked on</p>
+        </section>
+
+        <section class="page-section h-full">
+            <table class="min-w-full">
+                <thead>
+                    <tr>
+                        <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-black text-indigo-900 dark:text-indigo-200 sm:pl-6 md:pl-0">
+                            Year
+                        </th>
+                        <th scope="col" class="py-3.5 px-3 text-left text-sm font-black text-indigo-900 dark:text-indigo-200">Title</th>
+                        <th scope="col" class="py-3.5 px-3 text-left text-sm font-black text-indigo-900 dark:text-indigo-200">Description</th>
+                        <th scope="col" class="py-3.5 px-3 text-left text-sm font-black text-indigo-900 dark:text-indigo-200">Made at</th>
+                        <th scope="col" class="py-3.5 px-3 text-left text-sm font-black text-indigo-900 dark:text-indigo-200">Build with</th>
+                        <th scope="col" class="py-3.5 px-3 text-left text-sm font-black text-indigo-900 dark:text-indigo-200">Link</th>
+                    </tr>
+                </thead>
+                <tbody class="">
+                    <tr v-for="project in projects">
+                        <td
+                            class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-indigo-900 sm:pl-6 md:pl-0 font-space-mono dark:text-indigo-500"
+                        >
+                            {{ project.year }}
+                        </td>
+                        <td class="whitespace-nowrap py-4 px-3 text-sm font-black text-indigo-500 dark:text-indigo-100">{{ project.name }}</td>
+                        <td class="whitespace-normal py-4 px-3 text-sm text-indigo-900 dark:text-indigo-100">
+                            {{ project.description }}
+                        </td>
+                        <td class="whitespace-nowrap py-4 px-3 text-sm text-indigo-900 dark:text-indigo-100">{{ project.firm }}</td>
+                        <td class="stack whitespace-nowrap py-4 px-3 text-xs text-indigo-900 dark:text-indigo-100 font-space-mono">
+                            <span v-for="tag in project.stack" class="mx-1">{{ tag }}</span>
+                        </td>
+                        <td class="flex whitespace-nowrap py-4 px-3 text-sm text-indigo-900 dark:text-indigo-100">
+                            <div v-for="link in project.links">
+                                <a :href="link.link" target="_blank">
+                                    <icon v-if="link.type === 'website'" name="gridicons:domains" />
+                                    <icon v-if="link.type === 'github'" name="eva:github-outline" />
+                                </a>
+                            </div>
+                        </td>
+                    </tr>
+
+                    <!-- More people... -->
+                </tbody>
+            </table>
+        </section>
+    </main>
+</template>
+
+<script setup>
+    const config = useAppConfig();
+    const projects = config.projects;
+</script>
+
+<style scoped>
+    .stack span:after {
+        content: '\00B7';
+        padding-left: 5px;
+    }
+
+    .stack span:last-child:after {
+        content: '';
+    }
+</style>
